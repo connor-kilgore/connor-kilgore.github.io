bdf64fc477e43d23e8e6ee630e948679477a6ab2
Connor Kilgore <connor.kilgore@gmail.com>
2025-06-14 17:27:57 -0700

    removed cache files

--- _site/ttt/ttt-src/target/tic_tac_toe/core_spec.cljs
+++
@@ -1,7 +0,0 @@
-(ns tic-tac-toe.core-spec
-  (:require-macros [speclj.core :refer [describe context it should should-not should= run-specs focus-it]])
-  (:require [speclj.core]
-            [tic-tac-toe.main :as sut]))
-
-(describe "Core"
-  )
--- _site/ttt/ttt-src/target/tic_tac_toe/main.cljs
+++
@@ -1,8 +0,0 @@
-(ns tic-tac-toe.main
-  (:require [tic-tac-toe.ui-interface :as ui]
-            [tic-tac-toe.web-ui]))
-
-
-
-(defn main [& args]
-  (ui/initialize-ui {:ui :web-ui}))
